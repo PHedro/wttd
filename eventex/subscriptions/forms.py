7e94856fd3225d3ea252f2c38d2a7609ca2880a2
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2014-02-14 21:13:40 -0200

    form basico

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,5 +1,9 @@
 #coding: utf-8
+from django import forms
 
 
-class SubscriptionForm(object):
-    pass
+class SubscriptionForm(forms.Form):
+    name = forms.ChoiceField()
+    cpf = forms.CharField()
+    email = forms.EmailField()
+    phone = forms.CharField()
