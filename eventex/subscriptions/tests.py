dad16e8f3e127aad15dce70e58f708b8d096359d
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2014-02-14 20:59:29 -0200

    inicio do form de inscricao

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscribeTest(TestCase):
@@ -16,7 +17,11 @@ class SubscribeTest(TestCase):
         response = self.response
         self.assertContains(response, '<form')
         self.assertContains(response, '</form>')
-        self.assertContains(response, '<input', 5)
+        self.assertContains(response, '<input', 6)
         self.assertContains(response, 'type="text"', 3)
         self.assertContains(response, 'type="email"')
         self.assertContains(response, 'type="submit"')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    return render(request, 'subscriptions/subscription_form.html')
+    return render(
+        request,
+        'subscriptions/subscription_form.html',
+        {'form': SubscriptionForm()}
+    )
